1c9f2d155a77b46f921c06a138d97ef5c9146e35
hilmanzu <hilmanzutech@gmail.com>
2024-11-08 09:24:37 +0800

    feat:

--- core/CreateJwt.cs
+++ core/CreateJwt.cs
@@ -33,8 +33,8 @@ public class JwtService
             }),
             Expires = DateTime.UtcNow.AddDays(7),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keys), SecurityAlgorithms.HmacSha256Signature),
-            Issuer = "GoArif.com",
-            Audience = "GoArif.com",
+            Issuer = "Twillink.com",
+            Audience = "Twillink.com",
         };
 
         return tokenDescriptor;
